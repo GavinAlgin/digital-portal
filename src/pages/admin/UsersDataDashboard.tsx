8ab32fe3fd78ce270a61ab9d949bfa6ada2aacb6
GavinAlgin <gavinkidart65@gmail.com>
2025-12-17 14:52:20 +0200

    Call for action

--- src/pages/admin/UsersDataDashboard.tsx
+++ src/pages/admin/UsersDataDashboard.tsx
@@ -132,7 +132,11 @@ export default function UserDashboard() {
                         <td className="px-4 py-3">{u.email}</td>
                         <td className="px-4 py-3">{u.role === "student" ? u.program : u.role}</td>
                         <td className="px-4 py-3 text-right">
-                          <button className="rounded-md px-3 py-1 text-sm border hover:bg-neutral-100">Edit</button>
+                          <button className="rounded-md px-3 py-1 text-sm border hover:bg-neutral-100">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                            </svg>
+                          </button>
                         </td>
                       </tr>
                     ))
